79b501e642d502a3af53f1e5682e370032dba121
Trang Ho <trangho@kms.com.vn>
2018-01-03 11:26:59 +0700

    Edit something

--- src/test/java/SeleniumExample.java
+++ src/test/java/SeleniumExample.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumExample {
+
+
+    xcxczczc
     public static void main(String[] args) {
         // Create a new instance of the Firefox driver
         // Notice that the remainder of the code relies on the interface,
